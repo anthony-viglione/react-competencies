62de91ebfe8934a789c535b1751209bc69793b24
Anthony Viglione <viglione2014.2015@gmail.com>
2019-05-31 17:54:56 -0600

    fixed query to join the data from two tables

--- db/single_post.sql
+++ db/single_post.sql
@@ -1,4 +1,5 @@
 SELECT title, img, content, username, profile_pic
-FROM helo_posts 
-JOIN helo_users
-ON  author_id;
+FROM helo_users 
+JOIN helo_posts
+ON helo_users.ID = helo_posts.author_id
+WHERE helo_posts.author_id = 1;
