1308cc69b6c47f3023e2db7e572909d6474b8a46
Anthony Viglione <viglione2014.2015@gmail.com>
2019-06-28 16:34:43 -0600

    altered hardcoded parameter to accept variable input

--- db/single_post.sql
+++ db/single_post.sql
@@ -2,4 +2,4 @@ SELECT title, img, content, username, profile_pic
 FROM helo_users 
 JOIN helo_posts
 ON helo_users.ID = helo_posts.author_id
-WHERE helo_posts.author_id = 1;
+WHERE helo_posts.author_id = ${id};
